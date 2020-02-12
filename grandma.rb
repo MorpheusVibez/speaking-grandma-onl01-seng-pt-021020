33147942e05800af249f970303bd4d8f9d2416a9
Andre Skeene <halodre24@gmail.com>
2020-02-12 15:30:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase == phrase.capitalize
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.downcase
+  elsif phrase == phrase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
