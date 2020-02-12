9f12baaf8052938be87456a2391d4959ec2ed13d
Andre Skeene <halodre24@gmail.com>
2020-02-12 16:38:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
+    ""
   else
     "I LOVE YOU TOO PUMPKIN!"
   end
