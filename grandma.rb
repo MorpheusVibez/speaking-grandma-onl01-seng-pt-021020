b90a26369c869030d9b30d029d5b9a305f6a51da
Andre Skeene <halodre24@gmail.com>
2020-02-12 15:27:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
-  else phrase == "WHAT DID YOU EAT TODAY?"
+  else phrase == phrase.downcase
     "NO, NOT SINCE 1938!"
   end
 end
